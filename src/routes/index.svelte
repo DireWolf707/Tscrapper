474ece883e9871c9c9504792bafc2ae0a6f85acc
DireWolf707 <rahulsood707@gmail.com>
2022-04-30 13:44:21 +0530

    added page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,16 @@
-<h1 class="text-4xl font-bold underline">
-    Hello world!
-</h1>
+<div class="flex-row justify-center m-8">
+    <p class="font-bold text-2xl m-8 text-center">
+        Welcome to Tscrapper, enter the course you wanna find and your email to send you result!
+    </p>
+    <form action="/submitted" method="post">
+        <div class="flex my-4">
+            <input type="text" placeholder="Type course here..." class="input input-bordered w-10/12 md:w-7/12 lg:w-5/12 mx-auto">
+        </div>
+        <div class="flex my-4">
+            <input type="email" placeholder="Type email here..." class="input input-bordered w-10/12 md:w-7/12 lg:w-5/12 mx-auto">
+        </div>
+        <div class="flex">
+            <button class="btn btn-wide mx-auto">Submit</button>
+        </div>
+    </form>
+</div>
---
+++ src/routes/submitted.svelte
@@ -0,0 +1,5 @@
+<div class="h-screen flex">
+    <p class="m-auto text-4xl">
+        Submitted successfully ;)
+    </p>
+</div>
